a4b64a825af7a22d2a3d39c06cd928a302fb6401
Matthew Rosser <pihundredpounds@gmail.com>
2024-08-16 20:05:44 +0100

    chore: Update npm dependencies and add @iconify/svelte and sass
    This commit updates the npm dependencies in the package.json file to their latest stable versions. It also adds the @iconify/svelte package and the sass package as peer dependencies. These changes aim to improve the project by ensuring compatibility with the latest versions of the dependencies and adding support for using icons and Sass in the project.

--- src/lib/form/inputs/Text.svelte
+++ src/lib/form/inputs/Text.svelte
@@ -27,26 +27,26 @@
 	{#if field.validate !== undefined}<span class="nfk-fieldset__field-label__required">*</span
 		>{/if}</label
 >
-<div class="relative">
-	{#if field.icon}
-		<Icon icon={field.icon} class="nfk-fieldset__field-input__icon-left" />
-	{/if}
-	<input
-		type={variant}
-		id={key}
-		name={key}
-		class={`nfk-fieldset__field-input ${variant === 'password' ? 'nfk-fieldset__field-input--icon-right' : ''} ${field.disabled ? 'cursor-not-allowed' : 'cursor-text'}`}
-		placeholder={field.placeholder}
-		value={field.defaultValue ?? ''}
-		aria-invalid={error ? 'true' : 'false'}
-		disabled={field.disabled}
-	/>
-	{#if field.variant === 'password'}
-		<button on:click={togglePassword} type="button" class="nfk-fieldset__field-input__icon-right">
-			<Icon icon={passwordReveal ? 'mdi:eye' : 'mdi:eye-off'} />
-		</button>
-	{/if}
-</div>
+
+{#if field.icon}
+	<Icon icon={field.icon} class="nfk-fieldset__field-input__icon-left" />
+{/if}
+<input
+	type={variant}
+	id={key}
+	name={key}
+	class={`nfk-fieldset__field-input ${variant === 'password' ? 'nfk-fieldset__field-input--icon-right' : ''} ${field.disabled ? 'cursor-not-allowed' : 'cursor-text'}`}
+	placeholder={field.placeholder}
+	value={field.defaultValue ?? ''}
+	aria-invalid={error ? 'true' : 'false'}
+	disabled={field.disabled}
+/>
+{#if field.variant === 'password'}
+	<button on:click={togglePassword} type="button" class="nfk-fieldset__field-input__icon-right">
+		<Icon icon={passwordReveal ? 'mdi:eye' : 'mdi:eye-off'} />
+	</button>
+{/if}
+
 {#if error}
 	<p id={`${key}-error`} class="nfk-fieldset__field-error">
 		{error}
